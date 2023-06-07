e7d54f0c0556528e515278e4a752f0c3601180e7
Marc-Olivier Derouin <marc-olivier.derouin@polymtl.ca>
2023-06-06 21:55:25 -0400

    Normalizing the documentation

--- src/school_grader/test_case.py
+++ src/school_grader/test_case.py
@@ -25,24 +25,31 @@ def timeout(timeout_time: Optional[int]):
     If the function execution takes longer than `timeout_time` seconds,
     the main thread of the program will be interrupted.
 
-    :param timeout_time: The number of seconds after which to interrupt the main thread.
-    :return: The decorated function.
+    Args:
+        timeout_time (int or float): The number of seconds after which to interrupt the main thread.
+
+    Returns:
+        callable: The decorated function.
     """
     def decorator(function: Callable):
         """
         The actual decorator function that sets the timer and raises an exception
         if the function execution takes longer than `timeout_time` seconds.
 
-        :param function: The function to be decorated.
-        :return: The decorated function `inner`.
+        Args:
+            function: The function to be decorated.
+
+        Returns:
+            The decorated function `inner`.
         """
         def inner(*args, **kwargs):
             """
             The decorated function that sets the timer and raises an exception
             if the function execution takes longer than `timeout_time` seconds.
 
-            :param *args: The positional arguments passed to the decorated function.
-            :param **kwargs: The keyword arguments passed to the decorated function.
+            Args:
+                *args: The positional arguments passed to the decorated function.
+                **kwargs: The keyword arguments passed to the decorated function.
             """
             timer = None
             try:
@@ -83,14 +90,18 @@ class FileTestCase(TestCase):
 
     def __init__(self, test_name: str, file_name: str, expected_output: List[Union[str, Equality]], mock_input: List[str] = [], timeout_time: float = 1, fail_message: str = None):
         """
-        Initializes the test case with the given test name, file name, mock input, expected output, timeout_time and fail message
+        Initializes the test case with the given attributes.
 
-        :param test_name: name of the test
-        :param file_name: name of the python file to be tested
-        :param expected_output: list of str or Equality objects to be used as expected outputs
-        :param mock_input: list of strings to be used as inputs
-        :param timeout_time: maximum time in seconds that the test can run
-        :param fail_message: message to be displayed in case of failure
+        Args:
+            test_name (str): Name of the test.
+            file_name (str): Name of the Python file to be tested.
+            expected_output (list[str] or list[Equality]): List of str or Equality objects to be used as expected outputs.
+            mock_input (list[str]): List of strings to be used as inputs.
+            timeout_time (int or float): Maximum time in seconds that the test can run.
+            fail_message (str): Message to be displayed in case of failure.
+
+        Returns:
+            None
         """
         super().__init__(test_name, timeout_time, fail_message)
         self._file_name = file_name
@@ -98,6 +109,12 @@ class FileTestCase(TestCase):
         self._expected_output = expected_output
 
     def run_whole_file(self) -> None:
+        """
+        Run the whole file.
+
+        Returns:
+            None
+        """
         try:
             spec = importlib.util.spec_from_file_location(self._file_name, os.path.join(self._dirname, f'{self._file_name}.py'))
             module = importlib.util.module_from_spec(spec)
@@ -106,6 +123,12 @@ class FileTestCase(TestCase):
             raise FileNotFoundError(f'File {self._file_name}.py not found. Your current working directory is {self._dirname}.') from e
 
     def override_input(self) -> Callable[[], str]:
+        """
+        Override the input function to use the mock input.
+
+        Returns:
+            A function that returns the next mock input.
+        """
         queue = deque(self._mock_input)
         def fake_input(*_, **__) -> str:
             if not queue:
@@ -114,8 +137,20 @@ class FileTestCase(TestCase):
         return fake_input
 
     def runTest(self):
+        """
+        Run the test case.
+
+        Returns:
+            None
+        """
         @timeout(self._timeout_time)
         def exec_test():
+            """
+            Execute the test case.
+
+            Returns:
+                None
+            """
             with patch('builtins.input', self.override_input()), patch('sys.stdout', new=StringIO()) as fake_out:
                 try:
                     self.run_whole_file()
@@ -136,24 +171,21 @@ class FileTestCase(TestCase):
 
 
 class FunctionTestCase(TestCase):
-    """A basic test case class for running unit tests.
-
-    Attributes:
-        test_name (str): The name of the test case.
-        result_func (Callable): A function that returns the result to be tested.
-        expected_result: The expected result from the result_func.
-        timeout_time (float): The maximum time in seconds allowed for the test to complete.
-        fail_message (str, optional): A custom message to display on test failure. Default is None.
-    """
+    """A basic test case class for running unit tests."""
 
     def __init__(self, test_name: str, result_func: Callable, expected_result, timeout_time: float = 1, fail_message: str = None):
-        """Initialize the test case with the required attributes.
+        """
+        Initialize the test case with the required attributes.
 
-        :param test_name: The name of the test case.
-        :param result_func: A function that returns the result to be tested.
-        :param expected_result: The expected result from the result_func.
-        :param timeout_time: The maximum time in seconds allowed for the test to complete.
-        :param fail_message: A custom message to display on test failure. Default is None.
+        Args:
+            test_name (str): The name of the test case.
+            result_func (function): A function that returns the result to be tested.
+            expected_result: The expected result from the result_func.
+            timeout_time (int or float): The maximum time in seconds allowed for the test to complete.
+            fail_message (str, optional): A custom message to display on test failure. Default is None.
+
+        Returns:
+            None
         """
         super().__init__(test_name, timeout_time, fail_message)
         self._result_func = result_func
@@ -172,7 +204,11 @@ def run_tests(generate_html: bool = True):
     """
     Run all the tests in the BasicTestCase and FunctionTestCase classes.
 
-    :param generate_html: Whether to generate an HTML report of the test results. Default is True.
+    Args:
+        generate_html (bool): Whether to generate an HTML report of the test results. Default is True.
+
+    Returns:
+        bool: The value of the `generate_html` parameter indicating whether an HTML report is generated.
     """
     if not TestCase.tests_case:
         raise ValueError("No test cases were added to the test suite.")
@@ -185,17 +221,44 @@ def run_tests(generate_html: bool = True):
     suite.addTests(TestCase.tests_case)
 
     def json_runner() -> None:
+        """
+        Run the test suite and generate a JSON report of the results.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         with patch('sys.stdout', new=StringIO()) as _:
             runner = unittest.TextTestRunner(resultclass=JSONTestResult)
             result: JSONTestResult = runner.run(suite)
         result.print_report()
 
     def html_runner() -> None:
+        """
+        Run the test suite and generate an HTML report of the results.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         runner = unittest.TextTestRunner(resultclass=HTMLTestResult)
         result: HTMLTestResult = runner.run(suite)
         result.generate()
 
     def text_runner() -> None:
+        """
+        Run the test suite and generate a text report of the results.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         runner = unittest.TextTestRunner()
         runner.run(suite)
     
